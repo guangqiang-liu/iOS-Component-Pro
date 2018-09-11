288df5009e68c0a9f0d7421743ba707f4a103418
刘光强 <liuguangqiang@qianbaocard.com>
2018-09-11 17:16:18 +0800

    Initial Commit

---
+++ GQComponentDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GQComponentDemo
+//
+//  Created by 刘光强 on 2018/9/11.
+//  Copyright © 2018年 quangqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GQComponentDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GQComponentDemo
+//
+//  Created by 刘光强 on 2018/9/11.
+//  Copyright © 2018年 quangqiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
